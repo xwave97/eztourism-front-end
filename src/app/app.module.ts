43cdf90fcd3a310c11a150a32a90261313ca46a8
xwave97 <roma.loboda.2000@mail.ru>
2020-05-13 23:45:49 +0300

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http'
+import { RegistrationComponent } from './registration-component/registration.component';
+import { RouterModule, Routes } from '@angular/router';
+import {FormsModule} from "@angular/forms";
+import {HomeComponent} from './home-component/home.component';
+import {TourCompsComponent} from './tour-comps-component/tour-comps.component';
+import {TourCompSingleComponent} from './tour-comp-single-component/tour-comp-single.component';
+import {CookieService} from 'ngx-cookie-service';
+
+const appRoutes: Routes =[
+  {path: 'reg', component: RegistrationComponent},
+  {path: '', component: HomeComponent},
+  {path: 'tourComps', component: TourCompsComponent},
+  {path: 'singleTourComp', component: TourCompSingleComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    HomeComponent,
+    TourCompsComponent,
+    TourCompSingleComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
